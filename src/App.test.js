9ca9f0c0179d62be2fa7cbb78edd10e01747d958
Neo Malefetse <neomalefetse@gmail.com>
2020-12-28 15:41:16 +0200

    initial commit

---
+++ src/App.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import App from './App';
+
+// Feel free to use a different test framework you're more familiar with
+
+test('Test Template', () => {  
+  // const { getByText } = render(<App />);
+  // const linkElement = getByText();
+  // expect(linkElement).toBeInTheDocument();
+});
